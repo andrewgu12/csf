07547c5309614bfe0c410d8f04d8e7ddaad32f40
andrewgu12 <andrew@mingbogu.com>
2013-10-12 22:00:27 -0700

    cleaner menu with hover

--- index.php
+++ index.php
@@ -16,16 +16,17 @@
 					<img src="img/logo.png">
 				</span>
 				<span class="right">
-					<nav class="top-bar">
-						<section class="top-bar-section">
+					<nav class="nav-bar">
+						
 							<ul>
 								<li><a href="#">home</a></li>
 								<li><a href="#">about</a></li>
 								<li><a href="#">volunteer</a></li>
 								<li><a href="#">media</a></li>
-								<li><a href="#" >login</a></li>
+								<li><a href="#" >members</a></li>
+								<li><a href="#">join</a></li>
 							</ul>
-						</section>
+						
 					</nav>
 				</span>
 			</div>

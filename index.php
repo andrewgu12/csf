f691b2e828f26edaf8f2cf4c687b5033a78b21a8
andrewgu12 <andrew@mingbogu.com>
2013-10-13 21:50:30 -0700

    new footer completed

--- index.php
+++ index.php
@@ -42,7 +42,27 @@
 			</div>
 		</div>
 		<footer>
-			&copy; Monta Vista CSF 2013 | Facebook | Email | Website by Andrew Gu
+			<div id="footer-inner">
+				<div class="row">
+					<div class="large-6 columns">
+						<ul>
+							<li>&copy; Monta Vista CSF 2013</li>					
+							<li>Website by Andrew Gu</li>					
+						</ul>
+					</div>	
+					<div class="large-2  large-offset-2 columns">
+						<ul>
+							<li>About Us</li>
+							<li>Volunteer</li>					
+						</ul>
+					</div>
+					<div class="large-2 columns">
+						<ul>
+							<li>Facebook</li>
+							<li>Contact Us</li>
+					</div>
+				</div>
+			</div>
 		</footer>
 		<!--include all JS files for both personal and Foundation-->
 		 <script src="js/jquery.js"></script>  	

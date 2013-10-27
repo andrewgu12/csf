3c856ed11c58ddcde9cce649d3e18ed7467547d5
andrewgu12 <andrew@mingbogu.com>
2013-10-26 18:29:57 -0700

    admin backend created

---
+++ admin/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>CSF Admin Control Panel</title>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/app.css">	
+		<link rel="stylesheet" href="css/styles.css">
+		<script src="custom.modernizr.js"></script>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="banner">							
+				<img src="img/logo.jpg" />									
+			</div>
+			<div id="content">
+				<nav id="sidebar">
+					<ul>
+						<li>Home</li>
+						<li>Post News</li>
+						<li>All News</li>
+						<li>Create Member</li>
+						<li>View Members</li>
+						<li>Create Event</li>
+						<li>View Events</li>
+						<li>Closed Events</li>
+						<li>Data Reset</li>
+						<li>Logout</li>
+					</ul>
+				</nav>
+				<div id="mainContent">
+				</div>
+			</div>
+			<footer>
+				&copy; Monta Vista CSF 2013. All Rights Reserved.
+			</footer>
+		</div>
+		<script src="js/vendor/jquery.js"></script>
+		<script src="js/foundation/foundation.js"></script>
+	</body>
+</html>
